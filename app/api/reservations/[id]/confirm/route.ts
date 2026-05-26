bded5974202cee500fa0d85dfb30b109f09eec7b
Deepali <deepalikumari62004@gmail.com>
2026-05-26 13:25:46 +0530

    added confirm and release endpoint

---
+++ app/api/reservations/[id]/confirm/route.ts
@@ -0,0 +1,51 @@
+import {prisma} from"@/lib/prisma"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function POST (request : NextRequest, {params} : {params: {id: string}}) {
+    const {id} = await params;
+    try {
+        const reservation = await prisma.reservation.findFirst({
+            where: {id}
+        })
+
+        console.log('expiresAt value:', reservation?.expiresAt)
+        console.log('expiresAt type:', typeof reservation?.expiresAt)
+        console.log('now:', new Date())
+
+        if(!reservation) {
+           return NextResponse.json({message:"reservation not found"}, {status: 404})
+        }
+
+        if(new Date(reservation.expiresAt) < new Date()) {
+            return NextResponse.json({message: "reservation expired"}, {status : 410})
+        }
+
+        if(reservation.status !== 'PENDING'){
+            return NextResponse.json({message : "Order already confirmed"}, {status: 400})
+        }
+
+        const newReservation = await prisma.reservation.update({
+            where: {id},
+            data: {status :'CONFIRMED'}
+        })
+
+
+        await prisma.stock.update({
+            where :{ id: reservation.stockId},
+            data: {
+                reservedUnits: {decrement : reservation.quantity},
+                totalUnits: {decrement:reservation.quantity}
+        }
+        })
+
+        return NextResponse.json(newReservation, {status: 200})
+        
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({message : "Server error"}, {status: 500})
+        
+    }
+
+    
+
+}
---
+++ app/api/reservations/[id]/release/route.ts
@@ -0,0 +1,49 @@
+import {prisma} from "@/lib/prisma"
+import { NextResponse, NextRequest } from "next/server"
+
+export async function POST(request: NextRequest, {params} : {params:{id: string}}) {
+    const {id} = await params;
+    try {
+        const reservation = await prisma.reservation.findFirst({
+            where: {id}
+    
+        })
+    
+        if(!reservation) {
+            return NextResponse.json({message: "Reservation doesn't exist"},{status: 404} )
+        }
+
+        if (reservation.status !== 'PENDING') {
+            return NextResponse.json({ message: 'Reservation already actioned' }, { status: 400 })
+        }
+
+    
+        const newReservation = await prisma.reservation.update({
+            where: {id : id},
+            data:{
+                status: "RELEASED",
+    
+            }
+    
+        })
+        await prisma.stock.update({
+            where: {id: reservation.stockId},
+            data:{
+                reservedUnits:{ decrement: reservation.quantity},
+    
+            }
+    
+        })
+        return NextResponse.json(newReservation,{status: 200})
+    
+    
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message : "Error releasing the order"}, {status: 500})
+
+        
+    }
+    
+
+
+}
