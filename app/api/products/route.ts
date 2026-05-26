1e414d99c54d19153a794e2b576291ba3ede114c
Deepali <deepalikumari62004@gmail.com>
2026-05-26 13:56:56 +0530

    added expiry cleanup

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,6 +3,40 @@ import {NextResponse} from "next/server"
 
 
 export async function GET() {
+
+    const expiredReservations = await prisma.reservation.updateManyAndReturn({
+        where:{
+            status : "PENDING",
+            expiresAt: {lt : new Date()}
+
+        },
+        data: {
+            status: 'RELEASED'
+
+        },
+        select: {
+            id: true, 
+            stockId: true,
+            quantity: true
+        }
+    })
+
+    for (const reservation of expiredReservations) {
+        await prisma.stock.update({
+            where:{
+                id: reservation.stockId
+            },
+            data:{
+                reservedUnits: {decrement : reservation.quantity}
+            }
+        })
+
+
+    }
+
+
+
+
     const products = await prisma.product.findMany({
         include:{
             stocks: {
